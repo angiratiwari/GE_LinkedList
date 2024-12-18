6de6838b1d1b7f98b6448268766f9cca4e896679
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 01:09:51 +0530

    UC-1 added

---
+++ src/LinkedList.java
@@ -0,0 +1,27 @@
+public class LinkedList <T extends Comparable<T>> {
+    private Node<T> head;
+    // UC1: Create a simple Linked List
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Print the LinkedList
+    public void printList() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+}
